a68a46c43f7195dc6d15c77ebd686908ab2edded
niwrutti.ahire <niwtaxcaddy@gmail.com>
2026-09-02 21:15:33 +0530

    Project  Creation

---
+++ playwright.config.js
@@ -0,0 +1,43 @@
+// @ts-check
+const { defineConfig } = require('@playwright/test');
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// const dotenv = require('dotenv');
+// const path = require('path');
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+const config = defineConfig({
+  testDir: './tests',
+  timeout: 12000,
+  expect: {
+    timeout: 5000
+  },
+
+  //reporter: 'html',
+  reporter: [
+   ['line'],
+   ['allure-playwright']
+ ],
+
+  use: {
+    browserName: 'chromium',
+    headless: false,
+    screenshot: 'on',
+    trace: 'retain-on-failure',
+    viewport: null, // Uses full screen size
+    launchOptions: {
+      args: ["--start-maximized"],
+    },
+
+  },
+
+
+});
+module.exports = config;
+
