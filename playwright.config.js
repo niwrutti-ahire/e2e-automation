a6cb2b1962a9ecc7a6b7e70fd7c0814fdd016e1c
niwrutti.ahire <niwtaxcaddy@gmail.com>
2026-09-03 16:42:47 +0530

    allure report changes

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,7 @@ const config = defineConfig({
   //reporter: 'html',
   reporter: [
    ['line'],
-   ['allure-playwright']
+     ['allure-playwright', { outputFolder: 'allure-results' }]
  ],
 
   use: {
